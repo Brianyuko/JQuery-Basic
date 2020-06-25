66d6504afbfb1ef471317848c84f44f74de2f94e
Brianyuko <brianyuko45@gmail.com>
2020-06-25 10:55:45 +0700

    Css di JQuery

--- SelectionAndEvent.js
+++ SelectionAndEvent.js
@@ -9,13 +9,28 @@ $(document).ready(function(){
 
     //Event
     $('h1').click(function(){
-        $('h1').css('color', 'black');
-        $('img').css('width', '500');
+        // Property lebih dari satu
+        $('h1').css({
+            'color': 'white',
+            'fontSize': '30px',
+            backgroundColor: 'blue'
+        });
+        $('img').css('width', '500')
+        // Add Class
+        //$('#wrap_boxes').addClass('change');
+        
+        // Toogle Class , Toogle Add, Remove Class
+        $('#wrap_boxes').toggleClass('change');
     });
+
     $('.boxes').mouseenter(function(){
         $(this).css('color', 'black')
-    })
+    });
 
-    // 
+    $('img').click(function(){
+        //Remove Class
+        //$('#wrap_boxes').removeClass('change');
+        $(this).css('width', '300')
+    });
 });
 
